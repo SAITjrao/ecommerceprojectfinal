3af8d95af64b40c59716c154629a91514cf0460c
OnkarSingh-git <Onkar.Singh@edu.sait.ca>
2025-06-19 14:06:26 -0600

    route.js

--- app/api/me/route.js
+++ app/api/me/route.js
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const session = cookieStore.get("session");
 
   if (!session) {
